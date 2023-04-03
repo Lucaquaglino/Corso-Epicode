10d54361ac5304ae03ad042c5fa6d8bd461e6eb1
Luca Quaglino <quagli.luca@gmail.com>
2023-04-03 16:41:15 +0200

    esercizio 1 js

---
+++ Settimana2/Giorno1/assets/js/script1_pagina2.js
@@ -0,0 +1,5 @@
+/* SENZA VARIABILI*/
+document.getElementById('output').innerHTML='output scritto da Javascript';
+
+alert("ciao")
+window.alert("come stai?")
---
+++ Settimana2/Giorno1/assets/js/script2_pagina2.js
@@ -0,0 +1,16 @@
+/*CON VARIABILI*/
+
+var criceto = 'criceto';
+console.log('criceto');
+
+
+
+var giraffa ='giraffa';
+console.log('giraffa');
+
+
+var leone = 'leone';
+console.log('leone');
+
+var pantera = '<p> pantera scritto da JavaScript </p>'
+document.write( pantera );
