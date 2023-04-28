bfecd853503bd4a6e9e85727134092bdb0077f0a
Luca Quaglino <quagli.luca@gmail.com>
2023-04-28 19:03:48 +0200

    Progetto

---
+++ Progetto-settimana-5/assets/js/script.js
@@ -0,0 +1,24 @@
+const change = 400;
+const startedButtonChange = document.getElementById("startedButton");
+const navChanges = document.getElementById("header");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= change) {
+    startedButtonChange.classList.add("green-button");
+    navChanges.classList.add("navChange");
+    startedButtonChange.classList.remove("green-button-off");
+    navChanges.classList.remove("head-off");
+  } else if (window.screenY < change) {
+    startedButtonChange.classList.remove("green-button");
+    navChanges.classList.remove("navChange");
+    startedButtonChange.classList.add("green-button-off");
+    navChanges.classList.add("head-off");
+  }
+});
+
+// let m = document.querySelectorAll("g[stroke-linecap='butt']");
+// setInterval(() => {
+//   let random = Math.round(Math.random() * (m.length - 1));
+//   m[random].classList.toggle("invisible");
+// }, 10);
+
+
