cc786c52d4a7f9908eaba6157f99e40897638e8a
Luca Quaglino <quagli.luca@gmail.com>
2023-05-26 16:03:25 +0200

    1

--- Progetto-Settimana-9/assets/js/script.js
+++ Progetto-Settimana-9/assets/js/script.js
@@ -1,31 +1,41 @@
-//classe
 var User = /** @class */ (function () {
     function User() {
         this.carica = 0;
-        this.numeroChiamate = 0;
+        this.chiamate = [];
+        this.chiamataInCorso = null;
     }
     User.prototype.ricarica = function (unaRicarica) {
         this.carica += unaRicarica;
     };
-    User.prototype.chiamata = function (minutiDurata) {
-        var costoChiamata = 0.20 * minutiDurata;
+    User.prototype.iniziaChiamata = function () {
+        this.chiamataInCorso = { inizio: new Date(), fine: new Date() };
+    };
+    User.prototype.terminaChiamata = function () {
+        if (this.chiamataInCorso === null) {
+            console.error("Devi prima iniziare una chiamata.");
+            return;
+        }
+        this.chiamataInCorso.fine = new Date();
+        var durataChiamata = (this.chiamataInCorso.fine.getTime() - this.chiamataInCorso.inizio.getTime()) / 1000 / 60; // durata in minuti
+        var costoChiamata = 10 * durataChiamata;
         if (this.carica >= costoChiamata) {
             this.carica -= costoChiamata;
-            this.numeroChiamate += 1;
+            this.chiamate.push(this.chiamataInCorso);
         }
         else {
+            console.error(" Il tuo credito \u00E8 esaurito!!!");
             this.carica = 0;
-            this.numeroChiamate += 1;
         }
+        this.chiamataInCorso = null;
     };
     User.prototype.numero404 = function () {
         return this.carica;
     };
     User.prototype.getNumeroChiamate = function () {
-        return this.numeroChiamate;
+        return this.chiamate.length;
     };
     User.prototype.azzeraChiamate = function () {
-        this.numeroChiamate = 0;
+        this.chiamate = [];
     };
     return User;
 }());
@@ -35,32 +45,78 @@ var users = {
     "2": new User(),
     "3": new User()
 };
-//funzioni per prendere i valori dagli input in html 
 function ricarica() {
     var userId = Number(document.getElementById("users").value);
     var value = Number(document.getElementById("ricarica").value);
     var user = users[userId];
     user.ricarica(value);
-}
-function chiamata() {
-    var userId = Number(document.getElementById("users").value);
-    var value = Number(document.getElementById("chiamata").value);
-    var user = users[userId];
-    user.chiamata(value);
+    value = 0;
 }
 function azzeraChiamate() {
     var userId = Number(document.getElementById("users").value);
     var user = users[userId];
     user.azzeraChiamate();
+    var durataChiamataElement = document.getElementById("durataChiamata");
+    durataChiamataElement.innerText = "";
+    var numeroChiamateElement = document.getElementById("numeroChiamate");
+    numeroChiamateElement.innerText = "";
 }
 function checkUser() {
-    var userId = document.getElementById("users").value;
+    var userId = Number(document.getElementById("users").value);
     var user = users[userId];
+    var saldoResiduoElement = document.getElementById("saldoResiduo");
+    var numeroChiamateElement = document.getElementById("numeroChiamate");
     if (user) {
-        var saldo = user.numero404();
-        console.log("L'utente ".concat(userId, " ha un saldo residuo di: ").concat(saldo, " euro e ha effettuato ").concat(user.getNumeroChiamate(), " chiamate."));
-        if (saldo <= 0) {
-            console.log("L'utente ".concat(userId, " ha esaurito il credito."));
+        saldoResiduoElement.innerText = "Il tuo saldo residuo \u00E8 di: ".concat(user.numero404().toFixed(2), " euro.");
+        numeroChiamateElement.innerText = "Hai effettuato ".concat(user.getNumeroChiamate(), " chiamate.");
+        console.log("Il credito dell'utente ".concat(userId, " \u00E8: ").concat(user.numero404().toFixed(2), " \u20AC"));
+        console.log("L'utente ".concat(userId, " ha effettuato ").concat(user.getNumeroChiamate(), " chiamate."));
+        user.chiamate.forEach(function (chiamata, i) {
+            var durataChiamata = (chiamata.fine.getTime() - chiamata.inizio.getTime()) / 1000 / 60; // durata in minuti
+            var durataChiamataElement = document.getElementById("durataChiamata");
+            durataChiamataElement.innerText = "Chiamata ".concat(i + 1, ": Durata ").concat(durataChiamata.toFixed(2), " minuti");
+            console.log("Chiamata ".concat(i + 1, ": Durata ").concat(durataChiamata.toFixed(2), " minuti"));
+        });
+        if (user.numero404() === 0) {
+            console.error("L'utente ".concat(userId, " ha esaurito il credito."));
+        }
+    }
+}
+var inizioChiamata = null;
+var timerIntervalId = null;
+var timerSeconds = 0;
+function iniziaChiamata() {
+    var userId = Number(document.getElementById("users").value);
+    var user = users[userId];
+    user === null || user === void 0 ? void 0 : user.iniziaChiamata();
+    timerSeconds = 0;
+    timerIntervalId = window.setInterval(function () {
+        timerSeconds++;
+        //AGGIORNA IL TEMPO DEL TIMER
+        var timerDisplay = document.getElementById("timerDisplay");
+        timerDisplay.style.display = "block";
+        if (timerDisplay) {
+            timerDisplay.innerText = "Telefonata: ".concat(timerSeconds, " secondi");
+            var cardbody = document.getElementById("card");
+            if (cardbody) {
+                cardbody.style.backgroundColor = "green";
+            }
+        }
+    }, 1000);
+}
+function terminaChiamata() {
+    var userId = Number(document.getElementById("users").value);
+    var user = users[userId];
+    user === null || user === void 0 ? void 0 : user.terminaChiamata();
+    // FERMA TIMER E ELIMINALO
+    if (timerIntervalId !== null) {
+        window.clearInterval(timerIntervalId);
+        timerIntervalId = null;
+        var timerDisplay = document.getElementById("timerDisplay");
+        timerDisplay.style.display = "none";
+        var cardbody = document.getElementById("card");
+        if (cardbody) {
+            cardbody.style.backgroundColor = "black";
         }
     }
 }
--- Progetto-Settimana-9/assets/ts/script.ts
+++ Progetto-Settimana-9/assets/ts/script.ts
@@ -1,48 +1,70 @@
 
-//interfaccia 
-interface Smartphone {
-    carica: number; 
-    numeroChiamate: number; 
 
-    ricarica(unaRicarica: number): void;
-    chiamata(minutiDurata: number): void;
-    numero404(): number;
-    getNumeroChiamate(): number;
-    azzeraChiamate(): void;
+interface Chiamata {
+  inizio: Date;
+  fine: Date;
 }
-//classe
+
+interface Smartphone {
+  carica: number; 
+  chiamate: Chiamata[];
+
+  ricarica(unaRicarica: number): void;
+  iniziaChiamata(): void;
+  terminaChiamata(): void;
+  numero404(): number;
+  getNumeroChiamate(): number;
+  azzeraChiamate(): void;
+}
 
 class User implements Smartphone {
-    carica = 0; 
-    numeroChiamate = 0;
+  carica = 0; 
+  chiamate: Chiamata[] = [];
+  chiamataInCorso: Chiamata | null = null;
 
-    public ricarica(unaRicarica: number): void {
-        this.carica += unaRicarica;
-    }
+  public ricarica(unaRicarica: number): void {
+      this.carica += unaRicarica;
+  }
 
-    public chiamata(minutiDurata: number): void {
-        const costoChiamata = 0.20 * minutiDurata;
-        if (this.carica >= costoChiamata) {
-            this.carica -= costoChiamata;
-            this.numeroChiamate += 1;
-        } else {
-            this.carica= 0;
-            this.numeroChiamate += 1;
-        }
-    }
+  public iniziaChiamata(): void {
+      this.chiamataInCorso = { inizio: new Date(), fine: new Date() };
+  }
 
-    public numero404(): number {
-        return this.carica;
-    }
+  public terminaChiamata(): void {
+      if (this.chiamataInCorso === null) {
+          console.error("Devi prima iniziare una chiamata.");
+          return;
+      }
 
-    public getNumeroChiamate(): number {
-        return this.numeroChiamate;
-    }
+      this.chiamataInCorso.fine = new Date();
+      const durataChiamata = (this.chiamataInCorso.fine.getTime() - this.chiamataInCorso.inizio.getTime()) / 1000 / 60; // durata in minuti
+      const costoChiamata = 10 * durataChiamata;
 
-    public azzeraChiamate(): void {
-        this.numeroChiamate = 0;
-    }
+      if (this.carica >= costoChiamata) {
+          this.carica -= costoChiamata;
+          this.chiamate.push(this.chiamataInCorso);
+      } else {
+          console.error(` Il tuo credito è esaurito!!!`);
+          this.carica = 0;
+      }
+
+      this.chiamataInCorso = null;
+  }
+
+  public numero404(): number {
+      return this.carica;
+  }
+
+  public getNumeroChiamate(): number {
+      return this.chiamate.length;
+  }
+
+  public azzeraChiamate(): void {
+      this.chiamate = [];
+  }
 }
+
+
 
 //oggetto con le istanze 
 const users: { [key: string]: Smartphone } = {
@@ -53,40 +75,95 @@ const users: { [key: string]: Smartphone } = {
 };
 
 
- //funzioni per prendere i valori dagli input in html 
+
  
 function ricarica() {
   const userId = Number((<HTMLSelectElement>document.getElementById("users")).value);
   const value = Number((<HTMLInputElement>document.getElementById("ricarica")).value);
   const user = users[userId];
   user.ricarica(value);
+ 
 }
 
-function chiamata() {
-  const userId = Number((<HTMLSelectElement>document.getElementById("users")).value);
-  const value = Number((<HTMLInputElement>document.getElementById("chiamata")).value);
-  const user = users[userId];
-  user.chiamata(value);
-}
 
 function azzeraChiamate() {
   const userId = Number((<HTMLSelectElement>document.getElementById("users")).value);
   const user = users[userId];
   user.azzeraChiamate();
+  const durataChiamataElement = <HTMLParagraphElement>document.getElementById("durataChiamata");
+  durataChiamataElement.innerText = ``;
+  const numeroChiamateElement = <HTMLParagraphElement>document.getElementById("numeroChiamate");
+  numeroChiamateElement.innerText = ``;
 }
 
 
 function checkUser() {
-  const userId = (<HTMLSelectElement>document.getElementById("users")).value;
+  const userId = Number((<HTMLSelectElement>document.getElementById("users")).value);
   const user = users[userId];
+  const saldoResiduoElement = <HTMLParagraphElement>document.getElementById("saldoResiduo");
+  const numeroChiamateElement = <HTMLParagraphElement>document.getElementById("numeroChiamate");
+  
   if (user) {
-    const saldo = user.numero404();
-    console.log(`L'utente ${userId} ha un saldo residuo di: ${saldo} euro e ha effettuato ${user.getNumeroChiamate()} chiamate.`);
-    if (saldo <= 0) {
-      console.log(`L'utente ${userId} ha esaurito il credito.`);
-    }
+    saldoResiduoElement.innerText = `Il tuo saldo residuo è di: ${user.numero404().toFixed(2)} euro.`;
+    numeroChiamateElement.innerText = `Hai effettuato ${user.getNumeroChiamate()} chiamate.`;
+      console.log(`Il credito dell'utente ${userId} è: ${user.numero404().toFixed(2)} €`);
+      console.log(`L'utente ${userId} ha effettuato ${user.getNumeroChiamate()} chiamate.`);
+      
+      user.chiamate.forEach((chiamata, i) => {
+          const durataChiamata = (chiamata.fine.getTime() - chiamata.inizio.getTime()) / 1000 / 60; // durata in minuti
+          const durataChiamataElement = <HTMLParagraphElement>document.getElementById("durataChiamata");
+          durataChiamataElement.innerText = `Chiamata ${i + 1}: Durata ${durataChiamata.toFixed(2)} minuti`;
+          console.log(`Chiamata ${i + 1}: Durata ${durataChiamata.toFixed(2)} minuti`);
+      });
+
+      if (user.numero404() === 0) {
+          console.error(`L'utente ${userId} ha esaurito il credito.`);
+      }
   }
 }
 
+
+let inizioChiamata: Date | null = null;
+let timerIntervalId: number | null = null;
+let timerSeconds: number = 0;
+
+function iniziaChiamata() {
+  const userId = Number((<HTMLSelectElement>document.getElementById("users")).value);
+  const user = users[userId];
+  user?.iniziaChiamata();
+ 
+  timerSeconds = 0;
+  timerIntervalId = window.setInterval(() => {
+    timerSeconds++;
+    //AGGIORNA IL TEMPO DEL TIMER
+    const timerDisplay = document.getElementById("timerDisplay");
+    timerDisplay!.style.display = "block";
+    if (timerDisplay) {
+      timerDisplay.innerText = `Telefonata: ${timerSeconds} secondi`;
+        const cardbody = document.getElementById("card");
+        if(cardbody) {
+  cardbody.style.backgroundColor = "green";
+        }
+    }
+  }, 1000);
+}
+
+function terminaChiamata() {
+  const userId = Number((<HTMLSelectElement>document.getElementById("users")).value);
+  const user = users[userId];
+  user?.terminaChiamata();
+
+  // FERMA TIMER E ELIMINALO
+  if (timerIntervalId !== null) {
+    window.clearInterval(timerIntervalId);
+    timerIntervalId = null;
+    const timerDisplay = document.getElementById("timerDisplay");
+    timerDisplay!.style.display = "none";
+    const cardbody = document.getElementById("card");
+    if(cardbody) {
+cardbody.style.backgroundColor = "black";
+    }
+  }
+}
 
 
